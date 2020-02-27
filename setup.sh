524ea28bd42488aefc3803ba1ca3d642c998a81f
Yunus Emre Deligöz <ye@deligoz.me>
2020-02-28 01:05:07 +0300

    Add initial setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
