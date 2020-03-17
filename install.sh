abfb2549c25694fead72183f3286ecdc4eefefea
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-17 13:36:35 +0300

    install.sh: emojify

--- install.sh
+++ install.sh
@@ -2,7 +2,4 @@
 
 echo "Setting up your Mac..."
 
-echo "Check for Homebrew and install if we don't have it"
-if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+zsh brew.sh
