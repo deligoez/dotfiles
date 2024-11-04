87715893b20c8ee91fee3cf65d9d54eb1c6075cd
Yunus Emre Deligöz <ye@deligoz.me>
2024-11-04 21:30:23 +0300

    Add keyboard accessibility and performance settings
    Enabled full keyboard access for all controls to improve accessibility. Disabled press-and-hold for keys and set a very fast key repeat rate for enhanced typing performance.

--- osx.sh
+++ osx.sh
@@ -76,6 +76,16 @@ defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
 echo "🍏 Disable smart quotes as they’re annoying when typing code"
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
+echo "🍏 Enable full keyboard access for all controls"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+echo "🍏 Disable press-and-hold for keys in favor of key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+echo "🍏 Set a blazingly fast keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
 killall WindowManager
 killall Dock
 killall Finder
