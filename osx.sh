9803402b1aa59d870acbe073ee6eb51104dc6a4f
Yunus Emre Deligöz <ye@deligoz.me>
2024-11-04 22:06:56 +0300

    Refactor osx.sh with improved settings organization
    Split and categorized macOS configurations into relevant sections for better clarity and maintenance. Additionally, added new configurations for System Preferences, Login Window, Energy, Screen, and enhanced existing Finder settings.

--- osx.sh
+++ osx.sh
@@ -1,90 +1,176 @@
 echo "🍏🍏🍏 Setting up your Mac..."
 echo ""
 
+# Close open System Preferences panes, to prevent them from overriding settings.
+osascript -e 'tell application "System Preferences" to quit'
+
 ###############################################################################
 # WindowManager                                                               #
 ###############################################################################
 
-echo "🍏 Enable Standard Click to Show Desktop"
+echo "🍏 WindowManager: Enable Standard Click to Show Desktop"
 defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
 
 ###############################################################################
 # Dock                                                                        #
 ###############################################################################
 
-echo "🍏 Clear the dock"
+echo "🍏 Dock: Clear"
 defaults write com.apple.dock persistent-apps -array
 
-echo "🍏 Set the icon size of Dock items to 50 pixels"
+echo "🍏 Dock: Set the icon size of items to 50 pixels"
 defaults write com.apple.dock tilesize -int 50
 
-echo "🍏 Don’t animate opening applications from the Dock"
+echo "🍏 Dock: Don’t animate opening applications"
 defaults write com.apple.dock launchanim -bool false
 
-echo "🍏 Automatically hide and show the Dock"
+echo "🍏 Dock: Automatically hide and show"
 defaults write com.apple.dock autohide -bool true
 
-echo "🍏 Don’t show recent applications in Dock"
+echo "🍏 Dock: Don’t show recent applications"
 defaults write com.apple.dock show-recents -bool false
 
-echo "🍏 Speed up Mission Control animations"
+echo "🍏 Dock: Speed up Mission Control animations"
 defaults write com.apple.dock expose-animation-duration -float 0.1
 
-echo "🍏 Don’t automatically rearrange Spaces based on most recent use"
+echo "🍏 Dock: Don’t automatically rearrange Spaces based on most recent use"
 defaults write com.apple.dock mru-spaces -bool false
-
-###############################################################################
-# Finder                                                                      #
-###############################################################################
-
-echo "🍏 Set sidebar icon size to medium"
-defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
-
-echo "🍏 Expand save panel by default"
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
-
-echo "🍏 Expand print panel by default"
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-
-echo "🍏 Save to disk (not to iCloud) by default"
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
-echo "🍏 Automatically quit printer app once the print jobs complete"
-defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
-
-echo "🍏 Disable the “Are you sure you want to open this application?” dialog"
-defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 ###############################################################################
 # Input                                                                       #
 ###############################################################################
 
-echo "🍏 Disable auto-correct"
+echo "🍏 Input: Disable auto-correct"
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
-echo "🍏 Disable automatic capitalization as it’s annoying when typing code"
+echo "🍏 Input: Disable automatic capitalization as it’s annoying when typing code"
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 
-echo "🍏 Disable smart dashes as they’re annoying when typing code"
+echo "🍏 Input: Disable smart dashes as they’re annoying when typing code"
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
-echo "🍏 Disable automatic period substitution as it’s annoying when typing code"
+echo "🍏 Input: Disable automatic period substitution as it’s annoying when typing code"
 defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
 
-echo "🍏 Disable smart quotes as they’re annoying when typing code"
+echo "🍏 Input: Disable smart quotes as they’re annoying when typing code"
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
-echo "🍏 Enable full keyboard access for all controls"
+echo "🍏 Input: Enable full keyboard access for all controls"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-echo "🍏 Disable press-and-hold for keys in favor of key repeat"
+echo "🍏 Input: Disable press-and-hold for keys in favor of key repeat"
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-echo "🍏 Set a blazingly fast keyboard repeat rate"
+echo "🍏 Input: Set a blazingly fast keyboard repeat rate"
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+###############################################################################
+# Login Window                                                                #
+###############################################################################
+
+echo "🍏 LoginWindow: Show language menu in the top right corner of the boot screen"
+sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bool true
+
+###############################################################################
+# Energy                                                                      #
+###############################################################################
+
+echo "🍏 Energy: Disable machine sleep while charging"
+sudo pmset -c sleep 0
+
+###############################################################################
+# Screen                                                                      #
+###############################################################################
+
+echo "🍏 Screen: Require password immediately after sleep or screen saver begins"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+echo "🍏 Screen: Save screenshots to the desktop"
+defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+
+echo "🍏 Screen: Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)"
+defaults write com.apple.screencapture type -string "png"
+
+echo "🍏 Screen: Enable subpixel font rendering on non-Apple LCDs"
+# Reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
+defaults write NSGlobalDomain AppleFontSmoothing -int 1
+
+echo "🍏 Screen: Enable HiDPI display modes (requires restart)"
+sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+
+echo "🍏 Finder: Set sidebar icon size to medium"
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
+
+echo "🍏 Finder: Expand save panel by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+echo "🍏 Finder: Expand print panel by default"
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+echo "🍏 Finder: Save to disk (not to iCloud) by default"
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+echo "🍏 PrintingPrefs: Automatically quit printer app once the print jobs complete"
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+echo "🍏 LaunchServices: Disable the “Are you sure you want to open this application?” dialog"
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+echo "🍏 Finder: disable window animations and Get Info animations"
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+echo "🍏 Finder: Set Desktop as the default location for new Finder windows"
+defaults write com.apple.finder NewWindowTarget -string "PfHm"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+
+echo "🍏 Finder: show all filename extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo "🍏 Finder: show status bar"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo "🍏 Finder: show path bar"
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo "🍏 Finder: Display full POSIX path as Finder window title"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+echo "🍏 Finder: Keep folders on top when sorting by name"
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+echo "🍏 Finder: When performing a search, search the current folder by default"
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+echo "🍏 Finder: Disable the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+echo "🍏 Finder: Enable spring loading for directories"
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+
+echo "🍏 Finder: Remove the spring loading delay for directories"
+defaults write NSGlobalDomain com.apple.springing.delay -float 2
+
+echo "🍏 Finder: Avoid creating .DS_Store files on network or USB volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Disable disk image verification
+# defaults write com.apple.frameworks.diskimages skip-verify -bool true
+# defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+# defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+# Automatically open a new Finder window when a volume is mounted
+# defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+# defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+# defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
 killall WindowManager
 killall Dock
