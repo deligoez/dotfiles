5e43c62ebcefa33681e50a651ee2da4965c52353
Yunus Emre Deligöz <ye@deligoz.me>
2024-11-04 20:17:25 +0300

    Update Dock configurations and add new settings
    Enhanced the script to include several Dock-related settings, such as setting icon size, disabling animations, and auto-hiding the Dock. Also included options to speed up Mission Control animations and prevent automatic rearrangement of Spaces.

--- osx.sh
+++ osx.sh
@@ -1,8 +1,28 @@
-echo "✅ Enable Standard Click to Show Desktop"
+# Dock
+
+echo "🍏 Enable Standard Click to Show Desktop"
 defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
 
-echo "✅ Clear the dock"
+echo "🍏 Clear the dock"
 defaults write com.apple.dock persistent-apps -array
+
+echo "🍏 Set the icon size of Dock items to 50 pixels"
+defaults write com.apple.dock tilesize -int 50
+
+echo "🍏 Don’t animate opening applications from the Dock"
+defaults write com.apple.dock launchanim -bool false
+
+echo "🍏 Automatically hide and show the Dock"
+defaults write com.apple.dock autohide -bool true
+
+echo "🍏 Don’t show recent applications in Dock"
+defaults write com.apple.dock show-recents -bool false
+
+echo "🍏 Speed up Mission Control animations"
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+echo "🍏 Don’t automatically rearrange Spaces based on most recent use"
+defaults write com.apple.dock mru-spaces -bool false
 
 killall WindowManager
 killall Dock
