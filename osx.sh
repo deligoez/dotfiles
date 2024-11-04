f0e3d77466f9200f72dadaaf7869471ff956f4ab
Yunus Emre Deligöz <ye@deligoz.me>
2024-11-04 21:21:25 +0300

    Update setup scripts for better macOS configurations
    Enhanced macOS configuration scripts to include more Finder and Input settings, improving user experience by expanding panels by default and disabling various automatic text corrections. Modified Homebrew setup message for clarity.

--- osx.sh
+++ osx.sh
@@ -1,8 +1,16 @@
 echo "🍏🍏🍏 Setting up your Mac..."
 echo ""
 
+###############################################################################
+# WindowManager                                                               #
+###############################################################################
+
 echo "🍏 Enable Standard Click to Show Desktop"
 defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
+
+###############################################################################
+# Dock                                                                        #
+###############################################################################
 
 echo "🍏 Clear the dock"
 defaults write com.apple.dock persistent-apps -array
@@ -24,6 +32,49 @@ defaults write com.apple.dock expose-animation-duration -float 0.1
 
 echo "🍏 Don’t automatically rearrange Spaces based on most recent use"
 defaults write com.apple.dock mru-spaces -bool false
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+
+echo "🍏 Set sidebar icon size to medium"
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
+
+echo "🍏 Expand save panel by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+echo "🍏 Expand print panel by default"
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+echo "🍏 Save to disk (not to iCloud) by default"
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+echo "🍏 Automatically quit printer app once the print jobs complete"
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+echo "🍏 Disable the “Are you sure you want to open this application?” dialog"
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+###############################################################################
+# Input                                                                       #
+###############################################################################
+
+echo "🍏 Disable auto-correct"
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+echo "🍏 Disable automatic capitalization as it’s annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+echo "🍏 Disable smart dashes as they’re annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+echo "🍏 Disable automatic period substitution as it’s annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+echo "🍏 Disable smart quotes as they’re annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
 killall WindowManager
 killall Dock
